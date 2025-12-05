90fed3e675f12add9dde362e5439d26704e3a2b2
snsloe <ginkosan705@gmail.com>
2025-12-05 19:30:45 +0300

    upld files

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/api-explorer-hub-react/',
+    base: '/api-explorer-hub-react-2/',
 })
